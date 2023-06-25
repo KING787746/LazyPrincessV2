d547d267d41adc155e8a8921461d2651e06d85d8
KING787746 <136891895+KING787746@users.noreply.github.com>
2023-06-25 14:03:28 +0530

    Update Telegrambot.py

--- Telegrambot.py
+++ Telegrambot.py
@@ -1,35 +1,28 @@
-import sqlite3
 import telebot
 
-# Replace 'your_database_link' with the actual link or path to your database
-database_link = ''mongodb+srv://psychoexpertz78:psychoexpertz78@kingtry.prck45x.mongodb.net/?retryWrites=true&w=majority'
+# Replace 'your_token' with your actual bot token
+bot = telebot.TeleBot('5621047664:AAHFZPz2lGFzUC6gzKZJHiR7syXw_wfCtu8')
 
 # Replace 'your_channel_id' with the actual ID of your channel
 channel_id = '-1001939652477'
 
-# Create a Telegram bot instance
-bot = telebot.TeleBot('5621047664:AAHFZPz2lGFzUC6gzKZJHiR7syXw_wfCtu8')
-
 @bot.message_handler(commands=['getmovies'])
 def get_movies(message):
-    # Establish a connection to the database
-    conn = sqlite3.connect(database_link)
-    cursor = conn.cursor()
+    # Replace 'path_to_movie_files' with the actual path to your movie files directory
+    movie_files_path = 'path_to_movie_files'
 
-    # Fetch all movies from the database
-    cursor.execute("SELECT movie_name FROM movies")
-    movies = cursor.fetchall()
+    # Get a list of all movie files in the directory
+    import os
+    movie_files = [f for f in os.listdir(movie_files_path) if os.path.isfile(os.path.join(movie_files_path, f))]
 
-    # Close the database connection
-    cursor.close()
-    conn.close()
+    # Send each movie file to your channel
+    for movie_file in movie_files:
+        # Get the full file path
+        file_path = os.path.join(movie_files_path, movie_file)
 
-    # Send the movies' names to your channel
-    if movies:
-        movie_list = '\n'.join([movie[0] for movie in movies])
-        bot.send_message(channel_id, f"Here are all the movies:\n{movie_list}")
-    else:
-        bot.send_message(channel_id, "No movies found in the database.")
+        # Open and send the movie file
+        with open(file_path, 'rb') as file:
+            bot.send_document(channel_id, file)
 
 # Start the bot
 bot.polling()
